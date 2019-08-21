2063ee1ea93019dde2916f3712692de914bd8619
Travis Bischel <twmb@protonmail.com>
2019-08-21 04:15:02 -0400

    initial commit

---
+++ example_test.go
@@ -0,0 +1,88 @@
+package rbtree_test
+
+import (
+	"fmt"
+
+	"github.com/twmb/go-rbtree"
+)
+
+type Int int
+
+func (l Int) Less(r rbtree.Item) bool {
+	return l < r.(Int)
+}
+
+func ExampleIter() {
+	var r rbtree.Tree
+
+	for i := 0; i < 10; i++ {
+		r.Insert(Int(i)) // Int provides Less on int
+	}
+
+	// Declaring iter here to show we can reset it later.
+	var iter rbtree.Iter
+
+	// We can start iterating _at_ the max.
+	fmt.Println("iterating down...")
+	for iter = rbtree.IterAt(r.Max()); iter.Ok(); iter.Left() {
+		fmt.Println(iter.Item())
+	}
+
+	// Or, we can start iterating just before the min so that
+	// our first call to Right will start at the min.
+	iter.Reset(r.BeforeMin())
+	fmt.Println("iterating up...")
+	for {
+		next := iter.Right()
+		if next == nil { // when right returns nil, we are done
+			break
+		}
+		fmt.Println(next.Item)
+	}
+
+	// Putting it together...
+	fmt.Println("iterating down very inefficiently...")
+	for it := rbtree.IterAt(r.Min()); it.Ok(); it.Right() {
+		if it.Item() == r.Max().Item {
+			fmt.Println(it.Item())
+			r.Delete(it.Node())
+			it.Reset(r.BeforeMin())
+		}
+	}
+
+	// Output:
+	// iterating down...
+	// 9
+	// 8
+	// 7
+	// 6
+	// 5
+	// 4
+	// 3
+	// 2
+	// 1
+	// 0
+	// iterating up...
+	// 0
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+	// 6
+	// 7
+	// 8
+	// 9
+	// iterating down very inefficiently...
+	// 9
+	// 8
+	// 7
+	// 6
+	// 5
+	// 4
+	// 3
+	// 2
+	// 1
+	// 0
+
+}
---
+++ rbtree_test.go
@@ -0,0 +1,169 @@
+package rbtree
+
+import (
+	"math/rand"
+	"testing"
+)
+
+type myInt int
+
+func (l myInt) Less(r Item) bool {
+	return l < r.(myInt)
+}
+
+func TestRandom(t *testing.T) {
+	const bound = 50
+	rng := rand.New(rand.NewSource(0))
+	var r Tree
+	for i := 0; i < 10000; i++ {
+		r.Insert(myInt(rng.Intn(bound)))
+	}
+	for i := 0; i < 500; i++ {
+		if rng.Intn(3) == 0 {
+			if found := r.Find(myInt(rng.Intn(bound))); found != nil {
+				r.Delete(found)
+			}
+		} else {
+			r.Insert(myInt(rng.Intn(bound)))
+		}
+	}
+	for r.Len() > 0 {
+		if rng.Intn(2) == 0 {
+			r.Delete(r.Min())
+		} else {
+			r.Delete(r.Max())
+		}
+	}
+
+	if r.Min() != nil {
+		t.Error("expected len 0 min to be nil")
+	}
+	if r.Max() != nil {
+		t.Error("expected len 0 max to be nil")
+	}
+}
+
+type intPtr int
+
+func (l *intPtr) Less(r Item) bool {
+	return *l < *r.(*intPtr)
+}
+
+func TestFix(t *testing.T) {
+	newIntPtr := func(v int) *intPtr {
+		i := intPtr(v)
+		return &i
+	}
+
+	var r Tree
+	r.Insert(newIntPtr(1))
+	r.Insert(newIntPtr(2))
+	r.Insert(newIntPtr(3))
+	r.Insert(newIntPtr(4))
+	r.Insert(newIntPtr(9))
+	r.Insert(newIntPtr(8))
+	r.Insert(newIntPtr(7))
+	r.Insert(newIntPtr(6))
+	r.Insert(newIntPtr(5))
+
+	max := r.Max()
+	*max.Item.(*intPtr) = 0
+	r.Fix(max)
+
+	var exp intPtr
+	for iter := IterAt(r.Min()); iter.Ok(); iter.Left() {
+		if got := *iter.Item().(*intPtr); got != exp {
+			t.Errorf("got %d != exp %d", got, exp)
+		}
+		exp++
+	}
+}
+
+func TestIter(t *testing.T) {
+	var r Tree
+	r.Insert(myInt(0))
+	r.Insert(myInt(1))
+	r.Insert(myInt(2))
+	r.Insert(myInt(3))
+	r.Insert(myInt(4))
+	r.Insert(myInt(5))
+	r.Insert(myInt(6))
+	r.Insert(myInt(7))
+	r.Insert(myInt(8))
+	r.Insert(myInt(9))
+	const end = 10
+
+	iter := IterAt(r.BeforeMin())
+	for exp := 0; exp < end; exp++ {
+		if got := iter.Right().Item.(myInt); got != myInt(exp) {
+			t.Errorf("got %d != exp %d", got, exp)
+		}
+	}
+
+	iter.Reset(r.AfterMax())
+	for exp := end - 1; exp >= 0; exp-- {
+		if got := iter.Left().Item.(myInt); got != myInt(exp) {
+			t.Errorf("got %d != exp %d", got, exp)
+		}
+	}
+
+	iter.Reset(r.Min())
+	for exp := 0; exp < end; exp++ {
+		if got := iter.Item().(myInt); got != myInt(exp) {
+			t.Errorf("got %d != exp %d", got, exp)
+		}
+		iter.Right()
+	}
+
+	iter.Reset(r.Max())
+	for exp := end - 1; exp >= 0; exp-- {
+		if got := iter.Item().(myInt); got != myInt(exp) {
+			t.Errorf("got %d != exp %d", got, exp)
+		}
+		iter.Left()
+	}
+
+	var exp int
+	for iter := IterAt(r.Min()); iter.Ok(); iter.Right() {
+		if got := iter.Item().(myInt); got != myInt(exp) {
+			t.Errorf("got %d != exp %d", got, exp)
+		}
+		exp++
+	}
+
+	iter.Reset(r.AfterMax())
+	peek, left := iter.PeekLeft(), iter.Left()
+	if peek.Item != left.Item {
+		t.Error("destructive peek left")
+	}
+	if peek.Item != myInt(end-1) {
+		t.Errorf("got bad peek left from max %d", peek.Item)
+	}
+
+	iter.Reset(r.BeforeMin())
+	peek, right := iter.PeekRight(), iter.Right()
+	if peek.Item != right.Item {
+		t.Error("destructive peek right")
+	}
+	if peek.Item != myInt(0) {
+		t.Errorf("got bad peek right from min %d", peek.Item)
+	}
+}
+
+func BenchmarkInsertCase4(b *testing.B) {
+	b.Run("baseline", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			var r Tree
+			r.Insert(myInt(1))
+			r.Insert(myInt(2))
+		}
+	})
+	b.Run("thru_case4", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			var r Tree
+			r.Insert(myInt(1))
+			r.Insert(myInt(2))
+			r.Insert(myInt(3))
+		}
+	})
+}
